29a36bbf9a04eee196483f1652d646cdd1a0e32d
Piriya <pryeditor@gmail.com>
2025-04-18 22:21:52 +0700

    fix: fix table number

--- src/shared/pages/Summary/index.tsx
+++ src/shared/pages/Summary/index.tsx
@@ -147,18 +147,21 @@ export default function Summary({ itemArr, members }: SummaryProps) {
                       </td>
                       <td
                         className={`px-2 py-1 border border-gray-300 ${
-                          paid - shouldPay >= 0
+                          paid - shouldPay > 0
                             ? "bg-green-100 text-green-800"
-                            : "bg-red-100 text-red-800"
+                            : paid - shouldPay < 0
+                            ? "bg-red-100 text-red-800"
+                            : ""
                         }`}
                       >
-                        {paid - shouldPay >= 0 ? "+" : "-"}
+                        {paid - shouldPay > 0 && "+"}
+                        {paid - shouldPay < 0 && "-"}
                         {shouldPay.toLocaleString("en-US", {
                           minimumFractionDigits: 2,
                           maximumFractionDigits: 2,
                         })}
 
-                        {paid - shouldPay >= 0 && " (ได้คืน)"}
+                        {paid - shouldPay > 0 && " (ได้คืน)"}
                       </td>
                     </tr>
                   );
